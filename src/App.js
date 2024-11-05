e449b3912f2896eda39d64b86599e9bd8b3d7f2c
Alan Rincon <alan.rnca@gmail.com>
2024-11-05 16:53:27 -0600

    FEAT: Add template and logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [arrayInput, setArrayInput] = useState("");
+    const [numberInput, setNumberInput] = useState("");
+    const [result, setResult] = useState(null);
+
+    const arrayTimes = (array, number) => {
+        // const resultArray = [];
+        // array.forEach((item) => {
+        //     resultArray.push(item * number);
+        // });
+        // return resultArray;
+        return array.map((item) => item * number);
+    };
+
+    const handleClick = () => {
+        const array = arrayInput.split(",");
+        const output = arrayTimes(array, numberInput);
+        setResult(output);
+    };
+
+    return (
+        <div className="container">
+            <h2>Code Challenge P&G</h2>
+            <div className="input-group">
+                <input
+                    type="text"
+                    placeholder="Ingresa números separados por comas"
+                    value={arrayInput}
+                    onChange={(e) => setArrayInput(e.target.value)}
+                    className="input-field"
+                />
+                <input
+                    type="number"
+                    placeholder="Ingresa un número"
+                    value={numberInput}
+                    onChange={(e) => setNumberInput(e.target.value)}
+                    className="input-field"
+                />
+            </div>
+            <button onClick={handleClick} className="calculate-button">
+                Calcular
+            </button>
+            {result && <p className="result">Resultado: {result.join(", ")}</p>}
+        </div>
+    );
 }
 
 export default App;
